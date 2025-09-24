0f37d492f1ccc21b20a8c9a072f6e4de25474831
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2025-09-24 14:40:19 +0000

    Files for loading data

---
+++ AluminiumCylinder/io/file_paths.py
@@ -0,0 +1,55 @@
+#  Copyright 2024 - 2025 United Kingdom Research and Innovation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#   Authored by:    Laura Murgatroyd (UKRI-STFC)
+
+import os
+
+base_path = '/mnt/share/IMAT_share/Cylinder_Aluminium/CILZrO2_steel_trisospheres_test/Cropped'
+
+exp_3_75s_a_path = os.path.join(base_path, 'exp_3_75s_a')
+
+exp_3_75s_b_path = os.path.join(base_path, 'exp_3_75s_b')
+
+exp_3_75s_dark_B_path = os.path.join(base_path, 'Dark_Before_3_75s')
+exp_3_75s_dark_A_path = os.path.join(base_path, 'Dark_After_3_75s')
+
+exp_3_75s_flat_B_path = os.path.join(base_path, 'Flat_Before_3_75s')
+exp_3_75s_flat_A_path = os.path.join(base_path, 'Flat_after2_3_75s')
+
+exp_7_5s_path = os.path.join(base_path, 'exp_7_5s')
+
+exp_7_5s_dark_B_path = os.path.join(base_path, 'Dark_Before_7_5s')
+exp_7_5s_dark_A_path = os.path.join(base_path, 'Dark_After_7_5s')
+exp_7_5s_flat_B_path = os.path.join(base_path, 'Flat_Before_7_5s')
+exp_7_5s_flat_A_path = os.path.join(base_path, 'Flat_after2_7_5s')
+
+exp_15s_path = os.path.join(base_path, 'exp_15s')
+exp_15s_dark_B_path = os.path.join(base_path, 'Dark_Before_15s')
+exp_15s_dark_A_path = os.path.join(base_path, 'Dark_After_15s')
+exp_15s_flat_B_path = os.path.join(base_path, 'Flat_Before_15s')
+exp_15s_flat_A_path = os.path.join(base_path,  'Flat_after2_15s')
+
+exp_30s_path = os.path.join(base_path, 'exp_30s')
+exp_30s_dark_B_path = os.path.join(base_path, 'Dark_Before_30s')
+exp_30s_dark_A_path = os.path.join(base_path, 'Dark_After_30s')
+exp_30s_flat_B_path = os.path.join(base_path, 'Flat_Before_30s')
+exp_30s_flat_A_path = os.path.join(base_path,  'Flat_after2_30s')
+
+exp_3_75s_a_log_combined = os.path.join(base_path, 'IMAT00031028_CILZrO2_steel_trisospheres_testexp_3_75s_a_log_combined.csv')
+exp_3_75s_b_log_combined = os.path.join(base_path, 'IMAT00031028_CILZrO2_steel_trisospheres_testexp_3_75s_b_log_combined.csv')
+exp_7_5s_log_combined = os.path.join(base_path, 'IMAT00031028_CILZrO2_steel_trisospheres_testexp_7_5s_log_combined.csv')
+exp_15s_log_combined = os.path.join(base_path, 'IMAT00031028_CILZrO2_steel_trisospheres_testexp_15s_log_combined.csv')
+exp_30s_log_combined = os.path.join(base_path, 'IMAT00031028_CILZrO2_steel_trisospheres_testexp_30s_log_combined.csv')
+
